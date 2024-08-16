514dead75d28b28bd7bedf6abb83b5a9393f15b5
Muhammad Eshpulat <eshpulat.dev@gmail.com>
2024-08-16 11:42:37 -0400

    Add non blocking asynchronous way

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,8 +1,29 @@
 const fs = require("fs");
 
-const textIn = fs.readFileSync("./1-node-farm/txt/input.txt", "utf-8");
-console.log(textIn);
+// const textIn = fs.readFileSync("./1-node-farm/txt/input.txt", "utf-8");
+// console.log(textIn);
 
-const textOut = `This is what we know about the Avocado: ${textIn}.\nCreated on ${Date.now()}`;
-fs.writeFileSync("./1-node-farm/txt/output.txt", textOut);
-console.log("File Written!");
+// const textOut = `This is what we know about the Avocado: ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync("./1-node-farm/txt/output.txt", textOut);
+// console.log("File Written!");
+
+// non blocking asynchronous way
+
+fs.readFile("./1-node-farm/txt/start.txt", "utf-8", (err, Date1) => {
+    fs.readFile(`./1-node-farm/txt/${Date1}.txt`, "utf-8", (err, Date2) => {
+        console.log(Date2);
+        fs.readFile("./1-node-farm/txt/append.txt", "utf-8", (err, Date3) => {
+            console.log(Date3);
+
+            fs.writeFile(
+                "./1-node-farm/txt/final.txt",
+                `${Date2}\n${Date3}`,
+                "utf-8",
+                (err) => {
+                    console.log("Your file has been written");
+                }
+            );
+        });
+    });
+});
+console.log("will read this!");
