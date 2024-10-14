16929c58f3ec199b8532578e5e281a3c542a39f0
Muhammad Eshpulat <eshpulat.dev@gmail.com>
2024-10-14 11:45:42 -0400

    Add http server

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -36,28 +36,11 @@ const url = require("url");
 // ///////////////////////////////////////
 /////// SERVER
 
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
-const dataObj = JSON.parse(data);
-
 const server = http.createServer((req, res) => {
-    const pathName = req.url;
-
-    if (pathName === "/" || pathName === "/overview") {
-        res.end("this is OVERVIEW");
-    } else if (pathName === "/api") {
-        res.writeHead(200, { "Content-type": "application/json" });
-        res.end(data);
-    } else if (pathName === "/product") {
-        res.end("This is PRODUCT");
-    } else {
-        res.writeHead(404, {
-            "content-type": "text/html",
-            "my-own-header": "hello world!"
-        });
-        res.end("<h1>Page not fount</h1>");
-    }
+    console.log(req);
+    res.end("hello from the server!");
 });
 
 server.listen(8000, "127.0.0.1", () => {
-    console.log("Listening to request on port 8000 ");
+    console.log("listing to request on port 8000");
 });
