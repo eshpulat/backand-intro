e2d93b280dd3a46d300a32f95183975642f8be46
Muhammad Eshpulat <eshpulat.dev@gmail.com>
2024-10-21 11:40:03 -0400

    Add event loop section-4

---
+++ 2-how-node-works/event-loop.js
@@ -0,0 +1,12 @@
+const tempOverview = fs.readFileSync(
+    `${__dirname}/templates/tamplate-overview.html`,
+    "utf-8"
+);
+const tempCard = fs.readFileSync(
+    `${__dirname}/templates/template-card.html`,
+    "utf-8"
+);
+const tempProduct = fs.readFileSync(
+    `${__dirname}/templates/template-product.html`,
+    "utf-8"
+);
