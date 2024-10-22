13cdaf4ed5d1f9d559477dd684d61317c3b3ef1c
Muhammad Eshpulat <eshpulat.dev@gmail.com>
2024-10-22 16:39:23 -0400

    Add event loop

--- 2-how-node-works/event-loop.js
+++ 2-how-node-works/event-loop.js
@@ -1,12 +1,33 @@
-const tempOverview = fs.readFileSync(
-    `${__dirname}/templates/tamplate-overview.html`,
-    "utf-8"
-);
-const tempCard = fs.readFileSync(
-    `${__dirname}/templates/template-card.html`,
-    "utf-8"
-);
-const tempProduct = fs.readFileSync(
-    `${__dirname}/templates/template-product.html`,
-    "utf-8"
-);
+const fs = require("fs");
+const crypto = require("crypto");
+
+const start = Date.now();
+process.env.UV_THREADPOOL_SIZE = 3;
+
+setTimeout(() => console.log("time 1 finished"), 0);
+setImmediate(() => console.log("immediate 1 finished"));
+
+fs.readFile("test-file.txt", () => {
+    console.log("I/O finished");
+    console.log("____________________");
+
+    setTimeout(() => console.log("time 2 finished"), 0);
+    setTimeout(() => console.log("time 3 finished"), 3000);
+    setImmediate(() => console.log("immediate 2 finished"));
+
+    process.nextTick(() => console.log("process.nextTick"));
+
+    crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512");
+    console.log(Date.now() - start, "Password encrypted");
+
+    crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512");
+    console.log(Date.now() - start, "Password encrypted");
+
+    crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512");
+    console.log(Date.now() - start, "Password encrypted");
+
+    crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512");
+    console.log(Date.now() - start, "Password encrypted");
+});
+
+console.log("Hello from the top level code");
