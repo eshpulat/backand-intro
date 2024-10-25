e15d74cf946f8c57f60a0abb1ab03b3835167e85
Muhammad Eshpulat <eshpulat.dev@gmail.com>
2024-10-25 13:43:25 -0400

    Add events practice

--- 2-how-node-works/events.js
+++ 2-how-node-works/events.js
@@ -1,9 +1,46 @@
 const EventEmitter = require("events");
+const http = require("http");
 
-const myEmitter = new EventEmitter();
+class Sales extends EventEmitter {
+    constructor() {
+        super();
+    }
+}
+
+const myEmitter = new Sales();
 
 myEmitter.on("newSale", () => {
     console.log("there was new sale!");
 });
 
-myEmitter.emit("newSale");
+myEmitter.on("newSale", () => {
+    console.log("customer name : jonas");
+});
+
+myEmitter.on("newSale", (stock) => {
+    console.log(`there are now ${stock} itams left in stock`);
+});
+
+myEmitter.emit("newSale", 9);
+
+// ***************************** //
+
+const server = http.createServer();
+
+server.on("request", (req, res) => {
+    console.log("Request resived!");
+    console.log(req.url);
+    res.end("Request recived");
+});
+
+server.on("request", (req, res) => {
+    console.log("Another request 🥶");
+});
+
+server.on("close", () => {
+    console.log("server closed");
+});
+
+server.listen(8000, "127.0.0.1", () => {
+    console.log("waiting for requests...");
+});
