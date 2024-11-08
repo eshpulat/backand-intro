e99686efa9f8e6b9a57c7381e0c12f9bc493a902
Muhammad Eshpulat <eshpulat.dev@gmail.com>
2024-11-07 20:55:47 -0500

    Add 3 Part Middleware

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,7 +1,13 @@
 const fs = require("fs");
 const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
+
+//  1) MIDDLEWARE
+
+app.use(morgan("dev"));
+
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -17,6 +23,8 @@ app.use((req, res, next) => {
 const tours = JSON.parse(
     fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
+
+// 2) ROUTE HANDLERS
 
 const getAllTours = (req, res) => {
     console.log(req.requestTime);
@@ -104,12 +112,14 @@ const deleteTour = (req, res) => {
 // app.patch("/api/v1/tours/:id", UpdateTour);
 // app.delete("/api/v1/tours/:id", deleteTour);
 
+// 3) ROUTS
 app.route("/api/v1/tours").get(getAllTours).post(createTour);
 app.route("/api/v1/tours/:id")
     .get(getTour)
     .patch(UpdateTour)
     .delete(deleteTour);
 
+// 4) START SERVER
 const port = 3000;
 app.listen(port, () => {
     console.log(`App running on port ${port}...`);
