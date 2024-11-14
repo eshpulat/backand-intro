64c7dd0238fb6f1fa1025824be3852c86ae1aed5
Muhammad Eshpulat <eshpulat.dev@gmail.com>
2024-11-13 21:20:15 -0500

    Creating documents with  another way

--- 4-natours/controllers/tourControllers.js
+++ 4-natours/controllers/tourControllers.js
@@ -1,18 +1,18 @@
-const Tour = require('./../moduls/tourModel');
+const Tour = require('../moduls/tourModel');
 
 // const tours = JSON.parse(
 //   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`),
 // );
 
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'Missing name or price',
-    });
-  }
-  next();
-};
+// exports.checkBody = (req, res, next) => {
+//   if (!req.body.name || !req.body.price) {
+//     return res.status(400).json({
+//       status: 'fail',
+//       message: 'Missing name or price',
+//     });
+//   }
+//   next();
+// };
 
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
@@ -36,13 +36,21 @@ exports.getTour = (req, res) => {
   //   });
 };
 
-exports.createTour = (req, res) => {
-  res.status(201).json({
-    status: 'success',
-    // data: {
-    //   tours: newTour,
-    // },
-  });
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      massage: 'Invalid data Sent!',
+    });
+  }
 };
 
 exports.UpdateTour = (req, res) => {
