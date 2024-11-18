d6851b60e82283854959dea5ce259a7417c45cec
Muhammad Eshpulat <eshpulat.dev@gmail.com>
2024-11-17 21:58:57 -0500

    Making the API Better : Advanced Filtering

--- 4-natours/controllers/tourControllers.js
+++ 4-natours/controllers/tourControllers.js
@@ -17,11 +17,20 @@ const Tour = require('../moduls/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
+    // Filering
+
+    console.log(req.query);
+
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    const query = Tour.find(queryObj);
+    // Advansed Filtering
+
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    const query = Tour.find(JSON.parse(queryStr));
 
     // const tours = await Tour.find()
     //   .where('duration')
