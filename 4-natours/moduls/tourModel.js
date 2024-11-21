a7ef0aa555b2fba0b383ac2705f49dfcd953e475
Muhammad Eshpulat <eshpulat.dev@gmail.com>
2024-11-21 14:23:54 -0500

    Add Query Middleware

--- 4-natours/moduls/tourModel.js
+++ 4-natours/moduls/tourModel.js
@@ -56,6 +56,10 @@ const tourSchema = new mongoose.Schema(
       select: false,
     },
     startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     toJSON: { virtuals: true },
@@ -72,13 +76,27 @@ tourSchema.pre('save', function (next) {
   next();
 });
 
-tourSchema.pre('save', function (next) {
-  console.log('Will save documents...');
+// tourSchema.pre('save', function (next) {
+//   console.log('Will save documents...');
+//   next();
+// });
+
+// tourSchema.post('save', function (doc, next) {
+//   console.log(doc);
+//   next();
+// });
+
+// tourSchema.pre('find', function (next) {
+tourSchema.pre(/^find/, function (next) {
+  this.find({ secretTour: { $ne: true } });
+
+  this.start = Date.now();
   next();
 });
 
-tourSchema.post('save', function (doc, next) {
-  console.log(doc);
+tourSchema.post(/^find/, function (docs, next) {
+  console.log(`Query took ${Date.now() - this.start} millisecond!`);
+  console.log(docs);
   next();
 });
 
