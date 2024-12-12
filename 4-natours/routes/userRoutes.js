030d09d78fbf41ae2bbd0beea052591097e99f95
Muhammad Eshpulat <eshpulat.dev@gmail.com>
2024-12-11 19:05:12 -0500

    Add Creating New Users

--- 4-natours/routes/userRoutes.js
+++ 4-natours/routes/userRoutes.js
@@ -1,16 +1,19 @@
-const express = require("express");
-const userController = require("./../controllers/userControllers");
+const express = require('express');
+const userController = require('./../controllers/userControllers');
+const authController = require('../controllers/authController');
 
 const router = express.Router();
 
+router.post('/signup', authController.signup);
+
 router
-    .route("/")
-    .get(userController.getAllUsers)
-    .post(userController.createUser);
+  .route('/')
+  .get(userController.getAllUsers)
+  .post(userController.createUser);
 router
-    .route("/:id")
-    .get(userController.getUser)
-    .patch(userController.UpdateUser)
-    .delete(userController.deleteUser);
+  .route('/:id')
+  .get(userController.getUser)
+  .patch(userController.UpdateUser)
+  .delete(userController.deleteUser);
 
 module.exports = router;
