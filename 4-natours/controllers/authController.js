26f621fa6c1f507bc203c233d0da9a7e65c042d1
Muhammad Eshpulat <eshpulat.dev@gmail.com>
2024-12-13 14:03:14 -0500

    Add Protecting Tour Routes - Part 1

--- 4-natours/controllers/authController.js
+++ 4-natours/controllers/authController.js
@@ -48,3 +48,30 @@ exports.login = catchAsync(async (req, res, next) => {
     token,
   });
 });
+
+exports.protect = catchAsync(async (req, res, next) => {
+  // 1) getting token and check of it is there
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+  console.log(token);
+
+  if (!token) {
+    return next(
+      new AppError('Your are not logged in! Please log in to get access.', 401),
+    );
+  }
+
+  // 2) verification token
+
+  // 3) check if user still exists
+
+  // 4) check if user changed password after the token was issued
+
+  next();
+});
