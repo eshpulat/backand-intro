6f8c34a6c14d4d14f0859ef2b943b6b51577b432
Muhammad Eshpulat <eshpulat.dev@gmail.com>
2024-12-20 15:09:59 -0500

    Add Modelling Tour Guides: Embedding

--- 4-natours/moduls/tourModel.js
+++ 4-natours/moduls/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const User = require('./userModel');
 // const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
@@ -103,6 +104,7 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
+    guides: Array,
   },
   {
     toJSON: { virtuals: true },
@@ -120,15 +122,11 @@ tourSchema.pre('save', function (next) {
   next();
 });
 
-// tourSchema.pre('save', function (next) {
-//   console.log('Will save documents...');
-//   next();
-// });
-
-// tourSchema.post('save', function (doc, next) {
-//   console.log(doc);
-//   next();
-// });
+tourSchema.pre('save', async function (next) {
+  const guidesPromises = this.guides.map(async (id) => await User.findById(id));
+  this.guides = await Promise.all(guidesPromises);
+  next();
+});
 
 // tourSchema.pre('find', function (next) {
 tourSchema.pre(/^find/, function (next) {
