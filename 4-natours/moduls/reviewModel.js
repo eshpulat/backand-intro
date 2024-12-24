095cc14b017d5babd3451ef8375bff23598f61e9
Muhammad Eshpulat <eshpulat.dev@gmail.com>
2024-12-24 16:34:34 -0500

    Add Preventing Duplicate Reviews

--- 4-natours/moduls/reviewModel.js
+++ 4-natours/moduls/reviewModel.js
@@ -79,6 +79,8 @@ reviewSchema.statics.calcAverageRatings = async function (tourId) {
   }
 };
 
+reviewSchema.index({ tour: 1, user: 1 }, { unique: true });
+
 reviewSchema.post('save', function () {
   // This Points current review
   this.constructor.calcAverageRatings(this.tour);
