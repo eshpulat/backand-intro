06ccf70b2486be6f6a9e27edc5e0445a5d9cff52
Muhammad Eshpulat <eshpulat.dev@gmail.com>
2024-12-25 11:27:33 -0500

    Add Setting up Pug in Express

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -14,8 +15,13 @@ const reviewRouter = require('./routes/reviewRouts');
 
 const app = express();
 
+app.set('view-engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
 //  1)GLOBAL MIDDLEWARE
 // Set security HTTP headers
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(helmet());
 
 // Development loging
@@ -56,9 +62,6 @@ app.use(
   }),
 );
 
-// Serving static fileds
-app.use(express.static(`${__dirname}/public`));
-
 app.use((req, res, next) => {
   console.log('Hello from the middleware');
   next();
@@ -72,6 +75,9 @@ app.use((req, res, next) => {
 });
 
 // 3) ROUTS
+app.get('/', (req, res) => {
+  res.status(200).render('base');
+});
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
