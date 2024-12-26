d828395851aefb343b45f09ca2846780208d91b1
Muhammad Eshpulat <eshpulat.dev@gmail.com>
2024-12-26 11:58:12 -0500

    Building the Tour Overview - Part 1

--- 4-natours/controllers/viewsController.js
+++ 4-natours/controllers/viewsController.js
@@ -1,8 +1,19 @@
-exports.getOverviw = (req, res) => {
+const Tour = require('../moduls/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverviw = catchAsync(async (req, res, next) => {
+  // 1) Get tour data from collection
+  const tours = await Tour.find();
+
+  // 2) Build template
+
+  // 3) Render that template
+
   res.status(200).render('overview', {
     title: 'All tours',
+    tours,
   });
-};
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render('tour', {
