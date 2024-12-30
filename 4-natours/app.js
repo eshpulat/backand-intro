a1cc75fe83862e85eac170e4aa63a90417e43591
Muhammad Eshpulat <eshpulat.dev@gmail.com>
2024-12-29 21:49:32 -0500

    Update Logging in Users with Our API - Part 1

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -6,6 +6,7 @@ const helmet = require('helmet');
 const mongoSanitaiz = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -41,7 +42,7 @@ app.use('/api', limiter);
 
 // Body parser, reading data from body into req.body
 app.use(express.json());
-//{ limit: '10kb ' }
+app.use(cookieParser());
 
 // Data sanitization against NoSQL query injection
 app.use(mongoSanitaiz());
@@ -71,7 +72,7 @@ app.use((req, res, next) => {
 // Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
-  // console.log(req.headers);
+  console.log(req.cookies);
   next();
 });
 
--- 4-natours/routes/viewRouts.js
+++ 4-natours/routes/viewRouts.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const viewsController = require('../controllers/viewsController');
+const authController = require('../controllers/authController');
 
 const router = express.Router();
 
@@ -11,7 +12,7 @@ const router = express.Router();
 // });
 
 router.get('/', viewsController.getOverviw);
-router.get('/tour/:slug', viewsController.getTour);
+router.get('/tour/:slug', authController.protect, viewsController.getTour);
 router.get('/login', viewsController.getLoginForm);
 
 module.exports = router;
