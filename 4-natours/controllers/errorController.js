d7864cbd102bf3838f185b6227bcf12a1b0dce15
Muhammad Eshpulat <eshpulat.dev@gmail.com>
2024-12-31 21:15:34 -0500

    Add Rendering Error Pages

--- 4-natours/controllers/errorController.js
+++ 4-natours/controllers/errorController.js
@@ -25,33 +25,59 @@ const handleJWTErrorr = () =>
 const handleJWTExpridErrorr = () =>
   new AppError('Your token has expired! Please log in again.', 401);
 
-const sendErrorDev = (err, res) => {
-  res.status(err.statusCode).json({
-    status: err.status,
-    error: err,
-    message: err.message,
-    stack: err.stack,
-  });
-};
-
-const sendErrorProd = (err, res) => {
-  if (err.isOperational) {
+const sendErrorDev = (err, req, res) => {
+  // API
+  if (req.originalUrl.startsWith('/api')) {
     res.status(err.statusCode).json({
       status: err.status,
+      error: err,
       message: err.message,
+      stack: err.stack,
     });
-
-    // Programming or other unknown error : don't leak error detailes
   } else {
+    // RENDERED WEBSITE
+    console.error('ERROR 🔥', err);
+    res.status(err.statusCode).render('error', {
+      title: 'Somthing went wrong',
+      msg: err.message,
+    });
+  }
+};
+
+const sendErrorProd = (err, req, res) => {
+  // A) API
+  if (req.originalUrl.startsWith('/api')) {
+    // Operational trusted error: send message to clind
+    if (err.isOperational) {
+      return res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
+      });
+    }
+    // B) Programming or other unknown error : don't leak error detailes
     // 1) Log error
     console.error('ERROR 🔥', err);
-
     // 2) send generic message
-    res.status(500).json({
+    return res.status(500).json({
       status: 'error',
       message: 'Something went very wrong!',
     });
   }
+  // B) RENDERED WEBSITE
+  if (err.isOperational) {
+    return res.status(err.statusCode).render('error', {
+      title: 'Somthing went wrong',
+      msg: err.message,
+    });
+  }
+  // B) Programming or other unknown error : don't leak error detailes
+  // 1) Log error
+  console.error('ERROR 🔥', err);
+  // 2) send generic message
+  return res.status(err.statusCode).render('error', {
+    title: 'Somthing went wrong',
+    msg: 'Please try again layter',
+  });
 };
 
 module.exports = (err, req, res, next) => {
@@ -61,11 +87,12 @@ module.exports = (err, req, res, next) => {
   err.status = err.status || 'error';
 
   if (process.env.NODE_ENV === 'development') {
-    sendErrorDev(err, res);
+    sendErrorDev(err, req, res);
   } else if (process.env.NODE_ENV === 'production') {
     // let error = { ...err };
 
     let error = JSON.parse(JSON.stringify(err));
+    error.message = err.message;
 
     if (error.name === 'CastError') error = handleCastErrorDB(error);
     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
@@ -73,6 +100,6 @@ module.exports = (err, req, res, next) => {
       error = handleValidationErrorDB(error);
     if (error.name === 'JsonWebTokenError') error = handleJWTErrorr();
     if (error.name === 'TokenExpiredError') error = handleJWTExpridErrorr();
-    sendErrorProd(error, res);
+    sendErrorProd(error, req, res);
   }
 };
--- 4-natours/controllers/viewsController.js
+++ 4-natours/controllers/viewsController.js
@@ -1,5 +1,6 @@
 const Tour = require('../moduls/tourModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 exports.getOverviw = catchAsync(async (req, res, next) => {
   // 1) Get tour data from collection
@@ -22,6 +23,9 @@ exports.getTour = catchAsync(async (req, res, next) => {
     fields: 'review rating user',
   });
 
+  if (!tour) {
+    return next(new AppError('There is no tour with that name.', 404));
+  }
   // 2) Build template
 
   // 3) Render template using data from 1)
